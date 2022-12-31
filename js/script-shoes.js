e65cae7245ee354eb2c98004bde8340884be5126
Halil Gorkem Herek <halilherek@gmail.com>
2022-12-31 13:02:07 +0300

    MetBer Customs Frontend

---
+++ js/script-shoes.js
@@ -0,0 +1,8 @@
+/*Sidenav*/
+function openNav() {
+    document.getElementById("YanMenu").style.width = "250px";
+}
+
+function closeNav() {
+    document.getElementById("YanMenu").style.width = "0";
+}
